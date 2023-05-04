e5e885fdb26d1b69370ea5613c2dbd42b7ecd278
ChristianFast <112157470+ChristianFast@users.noreply.github.com>
2023-05-04 21:55:14 +0200

    Added SecondActivity.java with it's layout activity_second.xml. Added OnClickListener to the button in main to open the second activity Added EditText and Button to activity_second.xml.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button vOpenButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +18,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Assigning vOpenButton and setting up an onClickListener to open the second activity
+        vOpenButton = findViewById(R.id.openSecondButton);
+        vOpenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 }

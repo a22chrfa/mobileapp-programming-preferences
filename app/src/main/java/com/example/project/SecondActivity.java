762600b434b3857c02cb2b5ec1f5466d44d9d482
ChristianFast <112157470+ChristianFast@users.noreply.github.com>
2023-05-04 22:13:56 +0200

    Button in second activity now opens main activity and terminates second activity (to avoid stacking).

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -2,13 +2,31 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private Button vSaveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        //Assigning vSaveButton and setting up an onClickListener to open mainactivty, and terminate current activity.
+        vSaveButton = findViewById(R.id.save_button);
+        vSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
